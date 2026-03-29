c78f0cd88172b6d2b2e082611d572c117a01e9a0
Azzkaaaa <19623121@std.stei.itb.ac.id>
2026-03-29 20:25:06 +0700

    chore: update dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
 
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation("androidx.compose.material:material-icons-extended")
 }
