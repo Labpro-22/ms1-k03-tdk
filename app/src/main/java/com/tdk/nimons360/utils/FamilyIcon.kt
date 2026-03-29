2efb46219a3078d60c52719581874297d93e8374
Azzkaaaa <19623121@std.stei.itb.ac.id>
2026-03-29 20:26:45 +0700

    feat: add family icon

---
+++ app/src/main/java/com/tdk/nimons360/utils/FamilyIcon.kt
@@ -0,0 +1,14 @@
+package com.tdk.nimons360.utils
+
+object FamilyIcons {
+    val urls = listOf(
+        "https://mad.labpro.hmif.dev/assets/family_icon_1.png",
+        "https://mad.labpro.hmif.dev/assets/family_icon_2.png",
+        "https://mad.labpro.hmif.dev/assets/family_icon_3.png",
+        "https://mad.labpro.hmif.dev/assets/family_icon_4.png",
+        "https://mad.labpro.hmif.dev/assets/family_icon_5.png",
+        "https://mad.labpro.hmif.dev/assets/family_icon_6.png",
+        "https://mad.labpro.hmif.dev/assets/family_icon_7.png",
+        "https://mad.labpro.hmif.dev/assets/family_icon_8.png",
+    )
+}
