9d214f21c16a53d33465ef7db27c14acd335be34
Azzkaaaa <19623121@std.stei.itb.ac.id>
2026-03-30 15:31:14 +0700

    feat: add GET /api/families/discover and GET /api/me/families to ApiService

--- app/src/main/java/com/tdk/nimons360/data/remote/ApiService.kt
+++ app/src/main/java/com/tdk/nimons360/data/remote/ApiService.kt
@@ -1,8 +1,10 @@
 package com.tdk.nimons360.data.remote
 
+import com.tdk.nimons360.data.model.DiscoverFamiliesResponse
 import com.tdk.nimons360.data.model.LoginRequest
 import com.tdk.nimons360.data.model.LoginResponse
 import com.tdk.nimons360.data.model.MeResponse
+import com.tdk.nimons360.data.model.MyFamiliesResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -16,4 +18,10 @@ interface ApiService {
 
     @GET("api/me")
     suspend fun getMe(): Response<MeResponse>
+
+    @GET("api/me/families")
+    suspend fun getMyFamilies(): Response<MyFamiliesResponse>
+
+    @GET("api/families/discover")
+    suspend fun getDiscoverFamilies(): Response<DiscoverFamiliesResponse>
 }
